8551b40f92c219b2bdd11a8eed7dea21f9ab17e2
Jamie Schembri <jamie@schembri.me>
2023-11-20 11:39:18 +0100

    chore: initial commit

---
+++ lib/release/please/ruby/tests.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "tests/version"
+
+module Release
+  module Please
+    module Ruby
+      module Tests
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/release/please/ruby/tests/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Release
+  module Please
+    module Ruby
+      module Tests
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
